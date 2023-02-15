411eb826e08490bf41440fea9581009ac974322b
aryono3188 <52363455+aryono3188@users.noreply.github.com>
2023-02-15 15:11:47 +0700

    Add files via upload

---
+++ src/test/java/com/belajarspringboot/demomvccrudstatis/DemoMvcCrudstatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belajarspringboot.demomvccrudstatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMvcCrudstatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
