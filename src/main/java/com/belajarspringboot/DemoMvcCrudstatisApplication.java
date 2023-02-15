33037d3b463b89815963875bdc9c736ba5bd5a71
aryono3188 <52363455+aryono3188@users.noreply.github.com>
2023-02-15 14:36:08 +0700

    Create DemoMvcCrudstatisApplication.java

---
+++ src/main/java/com/belajarspringboot/DemoMvcCrudstatisApplication.java
@@ -0,0 +1,13 @@
+package com.belajarspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMvcCrudstatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMvcCrudstatisApplication.class, args);
+	}
+
+}
